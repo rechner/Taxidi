920c19b8909424fa691dab45256c8d6c0ac98431
Zac <jkltechinc@gmail.com>
2012-03-18 01:22:25 +0100

    Added barcode stuff. Have some viable html nametag work now; still investigating solutions

---
+++ codebar/codebar.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""
+Generates and crops barcodes. Use code.gen("code39", "file.png", "HELLO")
+to save a cropped barcode to file.png.  (only PNG is implemented so far).
+
+Couldn't figure out how to pass image size to pybarcode, so it'll be done
+with PIL.  This'll also contain methods for generating QR codes, etc.
+(probably has something to do with quiet_zone, but we'd like to crop
+out the subtext.  (http://packages.python.org/pyBarcode/writers/index.html)
+Supported symbologies should at least support uppercase ASCII.
+
+Uses pybarcode (2.7.1 distribution), David Jane's fork of QRCode for python
+(pyqrcode.py, MIT), and Jostein Leira's Code128 implementation (code128.py)
+"""
+
+codes = ['qr', 'code39', 'code128'] # working/supported codes
+
+import os
+import Image
+import pyqrcode
+import barcode
+from barcode.writer import ImageWriter
+from code128 import Code128
+
+def gen(encoding, fileName, text):
+    """Returns 0 success; all else are errors."""
+    if encoding not in codes:
+        raise NotImplementedError("Unsupported encoding")
+        return 2
+
+    if encoding == 'qr':
+        #generate QR code
+        qr_image = pyqrcode.MakeQRImage(text, br = True)
+        fileHandle = file(fileName, 'w')
+        qr_image.save(fileHandle, 'PNG')
+        fileHandle.close()
+        return 0
+
+    elif encoding == 'code128':
+        #generate using Code128() by Jostein Leira
+        bar = Code128()
+        bar.getImage(text, fileName, 50)
+        return 0
+
+    else:
+        #generate barcode
+        pre = barcode.get_barcode(encoding, text, writer=ImageWriter())
+        save = pre.save(fileName)
+
+        out = Image.open(save)
+        #crop the image
+        if encoding == 'code39':
+            out = out.crop((28, 10, out.size[0]-24, 180))
+        os.unlink(save)
+        fileHandle = file(fileName, 'w')
+        out.save(fileHandle, 'PNG')
+        fileHandle.close()
+        return 0
+
+
+def supported():
+    """Returns supported barcode types in a list."""
+    return codes
+
+if __name__ == '__main__':
+    gen('code128', 'test128.png', 'testing')
+    gen('code39', 'test39.png', 'testing')
+    gen('qr', 'testQR.png', 'testing')
--- conf.py
+++ conf.py
@@ -2,6 +2,8 @@
 #-*- coding:utf-8 -*-
 
 # Read config.ini from the present working directory
+# values are read out section-at-a-time using in-house methods for convenience,
+# but can also be read outside the module.
 
 import os.path, sys
 import configobj
@@ -12,4 +14,37 @@ inifile = os.path.join(appPath, 'config.ini')
 
 logger = logging.getLogger(__name__)
 #logger.setLevel(logging.DEBUG)
-logger.info("Reading configuration from '{}'".format(inifile))
+logger.info("Reading configuration from '{0}'".format(inifile))
+config = configobj.ConfigObj(inifile)
+
+def as_bool(string):
+    """Returns True if the key contains a string that represents True, or is the True object.
+Returns False if the key contains a string that represents False, or is the False object.
+Raises a ValueError if the key contains anything else.
+
+Strings that represent True are not case sensitive, and include: true, yes, on, 1.
+(I couldn't get configobj's built in as_bool function to work. ~~Zac 24.02.2012) 
+    """
+    #true, yes, on, 1
+    try: #is it a string?
+        test = string.lower() # ignore the case.
+    except AttributeError:    # is an object, not string.
+        return bool(test)     # return bool value.
+    
+    if test == 'true' or test == 'yes' or test == 'on' or test == '1d':
+        return True
+    elif test == 'false' or test == 'no' or test == 'off' or test == '0':
+        return False
+    else:
+        raise ValueError('Mangled boolean representation "{0}"'.format(string))
+        
+# Read section.  Returns values as a converted list.
+def conf():
+    config['general']
+
+def main():
+    # read out configuration for debugging... and stuff.
+    pass
+
+if __name__ == "__main__":
+    main()
