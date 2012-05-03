5219cd11cbcdc1120f7c62f88315a4c378e75126
Zac <jkltechinc@gmail.com>
2012-05-04 00:07:06 +0200

    Just some experimental changes

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ import sysinfo
 logFile = 'error.log'  #send this somewhere else for production (/tmp/?)
 loglevel = cli.parse()
 
+#TODO: Fix logging.  For now the following code puts log on the console:
+#~ self.log = logging.getLogger(__name__)
+#~ self.log.setLevel(logging.DEBUG)
+#~ ch = logging.StreamHandler()
+#~ ch.setLevel(logging.DEBUG)
+#~ formatter = logging.Formatter('[%(asctime)s] %(module)-6s  [%(levelname)-8s]  %(message)s')
+#~ ch.setFormatter(formatter)
+#~ self.log.addHandler(ch)
+
 #set up local file logging
 logging.basicConfig(filename=logFile, filemode='a',
     format='[%(asctime)s] %(module)-6s  [%(levelname)-8s]  %(message)s',
@@ -61,36 +70,41 @@ logger.addHandler(ch)       # add ch to logger
 #logger.critical('critical message')
 
 import conf     #import settings for application and database
-                #do this first?  (for logfile settings?)
+                #~ #do this first?  (for logfile settings?)
+#~
+#~ logger.debug("Loading database module for SQLite3 ('sqlite')")
+#~ import dblib.sqlite as sqlite
+#~ try:
+    #~ logger.debug('Attempting to open datbase object.')
+    #~ db = sqlite.Database('users.db', logger)
+#~ except TypeError as e:
+    #~ logger.error('({0})'.format(e))
+    #~ logger.warn('Unable to open database (file write-protected?)')
+    #~ #display an error dialogue and exit
+#~
+#~
+#~ db.Register("Zac", "Sturgeon", "1993-05-13", "3174555832", "V-5832", "Blah")
+#~ a = db.GetAll()
+#~ for record in a:
+    #~ print record[1]+' '+record[2]
+#~
+#~ db.AddCategory(u"Cafe", 0)
+#~ db.AddCategory("Parking", 0)
+#~ db.AddCategory("Tech", 1)
+#~ print db.GetCategories()
+#~ db.DeleteCategory(2)
+#~ print "\n"
+#~ print db.GetCategories()
+#~ db.UpdateCategory(1, u"Café", 1)
+#~ print "\n"
+#~ print db.GetCategories()
+#~ db.DeleteCategory(0) #remove all
+#~ print "\n"
+#~ print db.GetCategories()
+#~ db.commit()
 
-logger.debug("Loading database module for SQLite3 ('sqlite')")
-import dblib.sqlite as sqlite
-try:
-    logger.debug('Attempting to open datbase object.')
-    db = sqlite.Database('users.db', logger)
-except TypeError as e:
-    logger.error('({0})'.format(e))
-    logger.warn('Unable to open database (file write-protected?)')
-    #display an error dialogue and exit
+import webcam
+store = webcam.Storage()
 
 
-db.Register("Zac", "Sturgeon", "1993-05-13", "3174555832", "V-5832", "Blah")
-a = db.GetAll()
-for record in a:
-    print record[1]+' '+record[2]
-
-db.AddCategory(u"Cafe", 0)
-db.AddCategory("Parking", 0)
-db.AddCategory("Tech", 1)
-print db.GetCategories()
-db.DeleteCategory(2)
-print "\n"
-print db.GetCategories()
-db.UpdateCategory(1, u"Café", 1)
-print "\n"
-print db.GetCategories()
-db.DeleteCategory(0) #remove all
-print "\n"
-print db.GetCategories()
-db.commit()
 exit()
