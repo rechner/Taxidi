a6d92d39f9da5bdac42dd97fd8b2520df36dc037
Zac Sturgeon <jkltechinc@gmail.com>
2013-02-24 13:42:10 -0500

    Improved printing daemon

--- printer-daemon.py
+++ printer-daemon.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 #TODO: Add parent, volunteer nametag printing
-#TODO: 
+#TODO: Implement printing between multiple hosts (PHP side?, avahi?)
 #TODO: Catch first-run config install
 
 from functools import partial
@@ -24,64 +24,162 @@ class PrinterServer(resource.Resource):
         self.printer = printing.Main()
 
     def render_GET(self, request):
+        if conf.as_bool(conf.config['printing']['enable']) == False:
+            request.setResponseCode(500, message='Printing disabled')
+            return json.dumps({'status': 'fail', 
+                    'reason' : 'Printing disabled in configuration'})
+
         if request.postpath[0] == "nametag":
             request.setHeader("Content-Type", "application/json")
-            if conf.as_bool(conf.config['printing']['enable']):
-                #do printing
-                try:
-                    theme = request.args["theme"][0]
-                    room = request.args["room"][0]
-                    first = request.args["first"][0]
-                    last = request.args["last"][0]
-                    code = request.args["code"][0]
-                    medical = request.args["medical"][0]
-                    secure = request.args["secure"][0]
-                except KeyError as e:
-                    request.setResponseCode(400, message=e)
-                    return json.dumps({'status' : 'fail',
-                           'reason' : 'Missing argument: '+str(e)})
+            
+            #do printing
+            try:
+                theme = request.args["theme"][0]
+                room = request.args["room"][0]
+                first = request.args["first"][0]
+                last = request.args["last"][0]
+                code = request.args["code"][0]
+                medical = request.args["medical"][0]
+                secure = request.args["secure"][0]
+            except KeyError as e:
+                request.setResponseCode(400, message=e)
+                return json.dumps({'status' : 'fail',
+                       'reason' : 'Missing argument: '+str(e)})
+                
+            try: #should be a bool
+                barcode = conf.as_bool(request.args["barcode"][0])
+            except (ValueError, KeyError) as e:
+                request.setResponseCode(400)
+                return json.dumps({'status' : 'fail',
+                                   'reason' : str(e)})
+            try: #generate the nametag
+                if theme == None: theme = 'default'
+                self.printer.nametag(theme=theme, room=room,
+                                     first=first, last=last,
+                                     code=code, medical=medical,
+                                     secure=secure, barcode=barcode)
+            except KeyError as e:
+                request.setResponseCode(500, message=e)
+                return json.dumps({'status' : 'fail',
+                       'reason' : 'Nametag generator: '+str(e)})
+            
+            try: #send to the configured printer
+                if conf.config['printing']['printer'] == '' and not debug:
+                    self.printer.printout() #Use default printer
+                elif conf.config['printing']['printer'] != '':
+                    self.printer.printout(
+                        printer=conf.config['printing']['printer'])
+                elif debug:
+                    self.printer.preview()
+                    sleep(2)
+            except printing.PrinterError as e:
+                request.setResponseCode(418, message="I'm a teapot: "
+                    .format(e))
+                return json.dumps({'status' : 'fail',
+                                   'reason' : str(e)})
+            finally:
+                request.setResponseCode(202, 
+                    message="Queued for printing")
+                self.printer.cleanup()
+                return json.dumps({'status' : 'okay'})
+
                     
-                try: #should be a bool
-                    barcode = conf.as_bool(request.args["barcode"][0])
-                except (ValueError, KeyError) as e:
-                    request.setResponseCode(400)
-                    return json.dumps({'status' : 'fail',
-                                       'reason' : str(e)})
-                try: #generate the nametag
-                    if theme == None: theme = 'default'
-                    self.printer.nametag(theme=theme, room=room,
-                                         first=first, last=last,
-                                         code=code, medical=medical,
-                                         secure=secure, barcode=barcode)
-                except KeyError as e:
-                    request.setResponseCode(500, message=e)
-                    return json.dumps({'status' : 'fail',
-                           'reason' : 'Nametag generator: '+str(e)})
+        elif request.postpath[0] == "parent":
+            request.setHeader("Content-Type", "application/json")
+            #print parent tag
+            try:
+                theme = request.args["theme"][0]
+                room = request.args["room"][0]
+                first = request.args["first"][0]
+                last = request.args["last"][0]
+                code = request.args["code"][0]
+                secure = request.args["secure"][0]
+                link = request.args["link"][0] #None is valid here
+            except KeyError as e:
+                request.setResponseCode(400, message=e)
+                return json.dumps({'status' : 'fail',
+                       'reason' : 'Missing argument: '+str(e)})
+            try: #should be a bool
+                barcode = conf.as_bool(request.args["barcode"][0])
+            except (ValueError, KeyError) as e:
+                request.setResponseCode(400)
+                return json.dumps({'status' : 'fail',
+                                   'reason' : str(e)})
+            
+            try:
+                self.printer.parent(theme=theme, room=room,
+                                    first=first, last=last,
+                                    code=code, secure=secure,
+                                    link=link, barcode=barcode)
+            except KeyError as e:
+                request.setResponseCode(500, message=e)
+                return json.dumps({'status' : 'fail',
+                       'reason' : 'Nametag generator: '+str(e)})
+            
+            try: #send to the configured printer
+                if conf.config['printing']['printer'] == '' and not debug:
+                    self.printer.printout() #Use default printer
+                elif conf.config['printing']['printer'] != '':
+                    self.printer.printout(
+                        printer=conf.config['printing']['printer'])
+                elif debug:
+                    self.printer.preview()
+                    sleep(2)
+            except printing.PrinterError as e:
+                request.setResponseCode(418, message="I'm a teapot: "
+                    .format(e))
+                return json.dumps({'status' : 'fail',
+                                   'reason' : str(e)})
+            finally:
+                request.setResponseCode(202, 
+                    message="Queued for printing")
+                self.printer.cleanup()
+                return json.dumps({'status' : 'okay'})
                 
-                try: #send to the configured printer
-                    if conf.config['printing']['printer'] == '' and not debug:
-                        self.printer.printout() #Use default printer
-                    elif conf.config['printing']['printer'] != '':
-                        self.printer.printout(
-                            printer=conf.config['printing']['printer'])
-                    elif debug:
-                        self.printer.preview()
-                        sleep(2)
-                except printing.PrinterError as e:
-                    request.setResponseCode(418, message="I'm a teapot: "
-                        .format(e))
-                    return json.dumps({'status' : 'fail',
-                                       'reason' : str(e)})
-                finally:
-                    request.setResponseCode(202, 
-                        message="Queued for printing")
-                    self.printer.cleanup()
-                    return json.dumps({'status' : 'okay'})
+        elif request.postpath[0] == "volunteer":
+            request.setHeader("Content-Type", "application/json")
+            #print volunteer nametag
+            try:
+                theme = request.args["theme"][0]
+                room = request.args["room"][0]
+                first = request.args["first"][0]
+                last = request.args["last"][0]
+                ministry = request.args["ministry"][0]
+            except KeyError as e:
+                request.setResponseCode(400, message=e)
+                return json.dumps({'status' : 'fail',
+                       'reason' : 'Missing argument: '+str(e)})
+            
+            try: #generate nametag
+                self.printer.volunteer(theme=theme, room=room,
+                                       first=first, last=last,
+                                       ministry=ministry)
+            except KeyError as e:
+                request.setResponseCode(500, message=e)
+                return json.dumps({'status' : 'fail',
+                       'reason' : 'Nametag generator: '+str(e)})
+                       
+            try: #send to the configured printer
+                if conf.config['printing']['printer'] == '' and not debug:
+                    self.printer.printout() #Use default printer
+                elif conf.config['printing']['printer'] != '':
+                    self.printer.printout(
+                        printer=conf.config['printing']['printer'])
+                elif debug:
+                    self.printer.preview()
+                    sleep(2)
+            except printing.PrinterError as e:
+                request.setResponseCode(418, message="I'm a teapot: "
+                    .format(e))
+                return json.dumps({'status' : 'fail',
+                                   'reason' : str(e)})
+            finally:
+                request.setResponseCode(202, 
+                    message="Queued for printing")
+                self.printer.cleanup()
+                return json.dumps({'status' : 'okay'})
+            
                 
-            else:
-                request.setResponseCode(500, message='Printing disabled')
-                return json.dumps({'status': 'fail', 
-                    'reason' : 'Printing disabled in configuration'})
         else:
             request.setResponseCode(418, message="I'm a teapot: ")
             request.setHeader("Content-Type", "text/html")
