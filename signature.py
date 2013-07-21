21de07c25c1b860ac28a0a14dd4a53658abfb0e1
Nathan Lex <ILikeSmashingStuff@gmail.com>
2013-07-20 23:11:33 -0500

    Work on signature codec.

--- signature.py
+++ signature.py
@@ -246,3 +246,166 @@ if __name__ == '__main__':
     frame = TestFrame()
     frame.Show()
     app.MainLoop()
+
+
+
+
+import zlib, base64
+
+def encode(lines):
+    if len(lines) == 0: return "0"
+    
+    data = []
+    a = [(lines[0][0], lines[0][1])]
+    for g in range(1, len(lines)):
+        if lines[g][0] == lines[g - 1][2] and lines[g][1] == lines[g - 1][3]:
+            if a == [] or lines[g][2] != a[len(a) - 1][0] or lines[g][3] != a[len(a) - 1][1]:
+                a.append((lines[g][2], lines[g][3]))
+        else:
+            data.append(a)
+            a = [(lines[g][0], lines[g][1]), (lines[g][2], lines[g][3])]
+    data.append(a)
+    
+    e = []
+    for c in range(len(data)):
+        d = [data[c][0]]
+        f = data[c][1]
+        m = None
+        for h in range(1, len(data[c])):
+            if data[c][h][0] != f[0] or data[c][h][1] != f[1]:
+                t = data[c][h][0] - f[0]
+                if t == 0:
+                    n = None
+                else:
+                    n = (data[c][h][1] - f[1]) / t
+                d.append(f)
+                m = n
+                f = data[c][h]
+        if f[0] != d[0][0] or f[1] != d[0][1]:
+            d.append(f)
+            e.append(d)
+    
+    l = [""]
+    k = [-1]
+    def j(l, k, a):
+        if (k[0] != -1):
+            l[0] += chr((k[0] << 4) + a)
+            k[0] = -1
+        else:
+            k[0] = a
+    j(l, k, 0)
+    
+    def g(l, k, j, a):
+        if 7 >= a:
+            j(l, k, a)
+        else:
+            a = bin(a)[2:]
+            a =a.zfill((len(a) // 3) * 3 + 3)
+            b = []
+            for c in range(len(a) - 1, 0, -3):
+                b.insert(0, a[c - 2: c + 1])
+            for a in range(len(b) -1):
+                j(l, k, int("1" + b[a], 2))
+            j(l, k, int("0" + b[len(b) - 1], 2))
+    
+    for b in range(len(e)):
+        g(l, k, j, len(e[b]) - 1)
+        g(l, k, j, e[b][0][0])
+        g(l, k, j, e[b][0][1])
+        for a in range(1, len(e[b])):
+            c = e[b][a][0] - e[b][a - 1][0]
+            d = e[b][a][1] - e[b][a - 1][1]
+            f = [["111", "000", "001"], ["110", "you found a fox in the code :3", "010"], ["101", "100", "011"]][cmp(0, c) + 1][cmp(0, d) + 1]
+            if abs(c) <= 1 and abs(d) <= 1:
+                j(l, k, int("0" + f, 2))
+            else:
+                j(l, k, int("1" + f, 2))
+                if 0 != c:
+                    g(l, k, j, abs(c))
+                if 0 != d:
+                    g(l, k, j, abs(d))
+    
+    def b95btoa(bin):
+        n = int(("_" + bin).encode("hex"), 16); b95 = ""
+        
+        while n > 0:
+            b95 += chr(int(n % 95 + 32))
+            n /= 95
+        
+        return b95[::-1]
+    
+    if k[0] != -1: j(l, k, 0)
+    #print("f" + base64.b64encode(l[0]))
+    
+    e = zlib.compress(l[0])[2:-4]
+    if len(e) < len(l[0]):
+        return "b" + b95btoa(e)
+    else:
+        #print l[0]
+        return "e" + b95btoa(l[0])
+
+def decode(data):
+    def inflate(z):
+        return zlib.decompress(z, -zlib.MAX_WBITS)
+    
+    def b64atob(b64):
+        return base64.b64decode(b64 + "=" * (4 - len(b64) % 4))
+    
+    def b95atob(b95):
+        n = 0; m = 1
+        
+        for c in b95[::-1]:
+            n += (ord(c) - 32) * m
+            m *= 95
+        
+        return hex(n)[4:-1].decode("hex")
+    
+    def unwrap(d):
+        return {
+            "0": lambda x: "\0",
+            "a": inflate,
+            "b": lambda x: inflate(b95atob(x)),
+            "c": lambda x: inflate(b64atob(x)),
+            "d": lambda x: x,
+            "e": b95atob,
+            "f": b64atob
+        }[d[0]](d[1:])
+    
+    nibs = sum([[(ord(c) & 240) >> 4, ord(c) & 15] for c in unwrap(data)], [])
+    
+    def readnum():
+        a = 0; b = 8
+        
+        while b & 8:
+            b = nibs.pop(0)
+            a <<= 3; a|= b & 7
+        
+        return a
+    
+    output = []
+    
+    for i in range(readnum()):
+        x = readnum()
+        y = readnum()
+        output.append((x, y, x, y))
+    
+    while len(nibs) > 0:
+        j = readnum()
+        if j > 0:
+            x = readnum()
+            y = readnum()
+            for c in range(j):
+                k = nibs.pop(0)
+                b = k & 7
+                g = { 0: 1, 1:  1, 2:  0, 3: -1, 4: -1, 5: -1, 6: 0, 7: 1 }[b]
+                b = { 0: 0, 1: -1, 2: -1, 3: -1, 4:  0, 5:  1, 6: 1, 7: 1 }[b]
+                if k & 8:
+                    if 0 != g: g *= readnum()
+                    if 0 != b: b *= readnum()
+                output.append((x, y, x, y))
+                x += g
+                y += b
+    
+    return output
+
+
